833877f59b8a61dd528710dd448fd989f28f6e39
Jorrit Salverda <jsalverda@travix.com>
2018-05-29 15:42:35 +0200

    vendor complete zerolog library

--- govendor.sh
+++ govendor.sh
@@ -1,5 +1,5 @@
 govendor init
-govendor fetch github.com/rs/zerolog/log@v1.8.0
+govendor fetch github.com/rs/zerolog@v1.8.0
 govendor fetch github.com/alecthomas/kingpin@v2.2.5
 govendor fetch github.com/stretchr/testify/assert@v1.1.4
 govendor fetch github.com/sethgrid/pester
